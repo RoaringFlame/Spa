3b2a14d38c1b63389e3cdc9d0fb82062547276a4
Heng Chen <chenheng120@126.com>
2017-01-22 02:52:27 +0800

    给聊天滑块添加点击事件处理程序

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -21,17 +21,20 @@ spa.shell = (function () {
             + '<div class="spa-shell-chat"></div>'
             + '<div class="spa-shell-modal"></div>',
 
-
-            chat_extend_time: 1000,
+            chat_extend_time: 250,
             chat_retract_time: 300,
             chat_extend_height: 450,
-            chat_retract_height: 15
+            chat_retract_height: 15,
+
+            chat_extended_title: '点击关闭',
+            chat_retracted_title: '点击展开'
         },
         stateMap = {
-            $container: null
+            $container: null,
+            is_chat_retracted: true
         },
         jqueryMap = {},
-        setJqueryMap, toggleChat, initModule;
+        setJqueryMap, toggleChat, onClickChat, initModule;
 
     setJqueryMap = function () {
         var $container = stateMap.$container;
@@ -56,6 +59,10 @@ spa.shell = (function () {
                 {height: configMap.chat_extend_height},
                 configMap.chat_extend_time,
                 function () {
+                    jqueryMap.$chat.attr(
+                        'title', configMap.chat_extended_title
+                    );
+                    stateMap.is_chat_retracted = false;
                     if (callback) {
                         callback(jqueryMap.$chat);
                     }
@@ -67,6 +74,10 @@ spa.shell = (function () {
             {height: configMap.chat_retract_height},
             configMap.chat_retract_time,
             function () {
+                jqueryMap.$chat.attr(
+                    'title', configMap.chat_retracted_title
+                );
+                stateMap.is_chat_retracted = true;
                 if (callback) {
                     callback(jqueryMap.$chat);
                 }
@@ -75,18 +86,20 @@ spa.shell = (function () {
         return true;
     };
 
+    onClickChat = function (event) {
+        toggleChat(stateMap.is_chat_retracted);
+        return false;
+    };
+
     initModule = function ($container) {
         stateMap.$container = $container;
         $container.html(configMap.main_html);
         setJqueryMap();
 
-        setTimeout(function(){
-            toggleChat(true);
-        },3000);
-
-        setTimeout(function(){
-            toggleChat(false);
-        },8000);
+        stateMap.is_chat_retracted = true;
+        jqueryMap.$chat
+            .attr('title', configMap.chat_retracted_title)
+            .click(onClickChat);
     };
 
     return {initModule: initModule};
